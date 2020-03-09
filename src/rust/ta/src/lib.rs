b38479a6f01fd15375e69aaf2a861c45223fca4b
Juan Leni <juan.leni@zondax.ch>
2020-03-10 00:32:55 +0100

    allow different targets and tests

--- src/rust/ta/src/lib.rs
+++ src/rust/ta/src/lib.rs
@@ -1,12 +1,27 @@
 #![no_std]
 #![no_builtins]
 
+#[cfg(not(test))]
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! { loop {} }
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
 
 #[no_mangle]
 pub extern "C" fn ta_test() -> u8 {
     42
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ta_test;
+
+    #[test]
+    fn test_local() {
+        let v = ta_test();
+        assert_eq!(v, 42);
+    }
 }
